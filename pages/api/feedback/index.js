114bb0a5d458ea6b9b6ee9fb2911712175ee6339
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-05-18 14:50:00 +0700

    change repo

--- pages/api/feedback/index.js
+++ pages/api/feedback/index.js
@@ -1,23 +1,9 @@
-import fs from 'fs';
-import path from 'path';
-
-export function buildFeedbackPath() {
-	return path.join(process.cwd(), 'data', 'feedback.json');
-}
-
-export function extractFeedback(filePath) {
-	const fileData = fs.readFileSync(filePath);
-	const data = JSON.parse(fileData);
-	return data;
-}
-
 function handler(req, res) {
-	console.log('Co request den');
+	console.log('Co request den roi');
 	console.log(req.body);
-	//console.log(req);
-	//const filePath = buildFeedbackPath();
-	//const data = extractFeedback(filePath);
-	res.status(200).json({ feedback: "OK" });
+	return res.status(200).send({
+		actions: [],
+	})
 }
 
 export default handler;
