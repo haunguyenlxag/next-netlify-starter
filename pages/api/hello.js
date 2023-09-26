72897c21cba39cc0ccc3cd7b58fcd7d15f3abe11
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-09-26 19:40:14 +0700

    update 2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import  { NextApiRequest, NextApiResponse } from 'next'
+import homepageController from "../../controllers/homepageController";
 
-
-
+import chatbotService from "../../services/chatbotService";
 export default function handler(
     req,
     res
 ) {
-    res.status(200).json({ name: 'John Doe' })
+
+     chatbotService.sendMessage('6617646054939282', {"text": "The super bot came back !!!"});
+    res.status(200).send('EVENT_RECEIVED');
+    //res.status(200).json({ name: 'John Doe' })
 }
