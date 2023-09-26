eda30c57f324ee3884b804db6551ecc876e43088
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-09-26 20:19:18 +0700

    update 3

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,12 +3,13 @@ import  { NextApiRequest, NextApiResponse } from 'next'
 import homepageController from "../../controllers/homepageController";
 
 import chatbotService from "../../services/chatbotService";
-export default function handler(
+export default async function handler(
     req,
     res
 ) {
 
-     chatbotService.sendMessage('6617646054939282', {"text": "The super bot came back !!!"});
+    let result = await chatbotService.sendMessage('6617646054939282', {"text": "The super bot came back !!!"});
+    console.log(result);
     res.status(200).send('EVENT_RECEIVED');
     //res.status(200).json({ name: 'John Doe' })
 }
